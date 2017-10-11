b038f6caf524f4d871168abbfa1bb39aa3d1d54e
Sean Wilkinson <swilkinson09@gmail.com>
2017-10-11 14:38:06 -0400

    Update tests to use enzyme Add flow command to circle.yml

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from '../containers/App';
 
+Enzyme.configure({ adapter: new Adapter() });
 it('root renders App without crashing', () => {
 	const root = document.createElement('root');
 	ReactDOM.render(<App />, root);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,14 @@
+// @flow
+
 import React from 'react';
-import logo from '../images/logo.svg';
 import './App.css';
 
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <h1 className="App-title">Welcome to React</h1>
     </header>
-    <p classNam e="App-intro">
+    <p className="App-intro">
       To get started, edit <code>src/App.js</code> and save to reload.
     </p>
   </div>
