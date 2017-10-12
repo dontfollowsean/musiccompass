cf75ca9e1d58a3cc63cf53681248fc318e3689c6
Sean Wilkinson <swilkinson09@gmail.com>
2017-10-12 17:30:49 -0400

    Add youtube video to landing page

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,13 +1,28 @@
 // @flow
 
 import React from 'react';
+import YouTube from 'react-youtube';
 import SearchBox from '../components/SearchBox';
+import './Landing.css';
+import '../utils/youtubePlayerApi';
+
+const videoOptions = {
+	playerVars: {
+		list: 'PLFgquLnL59alCl_2TQvOiD5Vgm1hCaGSI',
+		iv_load_policy: '3',
+		controls: '0',
+		autoplay: '1'
+	}
+};
 
 const Landing = () => (
 	<div className="landing">
 		<h1>Travel with music</h1>
 		<h3>Create the local playlist of your dreams. Try it! </h3>
 		<SearchBox />
+		<div className="video_container">
+			<YouTube opts={videoOptions} />
+		</div>
 	</div>
 );
 
